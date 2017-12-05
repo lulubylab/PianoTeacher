1bb880ac46648969eefe710463090f11a0247bbd
Moze <34182107+lulubylab@users.noreply.github.com>
2017-12-05 04:59:35 -0500

    updating!

--- pianoTeacher.js
+++ pianoTeacher.js
@@ -1,87 +1,168 @@
 
-
 class pianoTeacher{
   constructor(){
-    this.s= new createjs.Stage("pianoTeacher");
-    var pt = new createjs.Shape();
-    pt.graphics.beginFill("#086464").rect(0,0,1200,400);
-    pt.x = pt.y = 0;
-    this.s.addChild(pt);
-
-    var options = new createjs.Shape();
-    options.graphics.beginFill("#80A000").rect(1200,0,210,1410);
-    options.x = options.y = 0;
-    this.s.addChild(options);
-
-    var piano = new createjs.Shape();
-    pt.graphics.beginFill("#000000").rect(0,400,1200,220);
-    pt.x = pt.y = 0;
-    this.s.addChild(piano);
-
-    var c = new createjs.Shape();
-    c.graphics.beginStroke("black").beginFill("#FFFFFF").rect(0,400,171,220);
-    c.x = pt.y = 0;
-    this.s.addChild(c);
-
-    var d = new createjs.Shape();
-    d.graphics.beginFill("#FFFFFF").rect(171,400,171,220);
-    d.x = pt.y = 0;
-    this.s.addChild(d);
-
-    var e = new createjs.Shape();
-    e.graphics.beginStroke("black").beginFill("#FFFFFF").rect(342,400,171,220);
-    e.x = pt.y = 0;
-    this.s.addChild(e);
-
-    var f = new createjs.Shape();
-    f.graphics.beginStroke("black").beginFill("#FFFFFF").rect(513,400,171,220);
-    f.x = pt.y = 0;
-    this.s.addChild(f);
-
-    var g = new createjs.Shape();
-    g.graphics.beginStroke("black").beginFill("#FFFFFF").rect(684,400,171,220);
-    g.x = pt.y = 0;
-    this.s.addChild(g);
-
-    var a = new createjs.Shape();
-    a.graphics.beginStroke("black").beginFill("#FFFFFF").rect(855,400,171,220);
-    a.x = pt.y = 0;
-    this.s.addChild(a);
-
-    var b = new createjs.Shape();
-    b.graphics.beginStroke("black").beginFill("#FFFFFF").rect(1026,400,171,220);
-    b.x = pt.y = 0;
-    this.s.addChild(b);
-
-    var cs = new createjs.Shape();
-    cs.graphics.beginFill("#000000").rect(120,400,100,150);
-    cs.x = pt.y = 0;
-    this.s.addChild(cs);
-
-    var ds = new createjs.Shape();
-    ds.graphics.beginFill("#000000").rect(290,400,100,150);
-    ds.x = pt.y = 0;
-    this.s.addChild(ds);
-
-    var fs = new createjs.Shape();
-    fs.graphics.beginFill("#000000").rect(635,400,100,150);
-    fs.x = pt.y = 0;
-    this.s.addChild(fs);
-
-    var gs = new createjs.Shape();
-    gs.graphics.beginFill("#000000").rect(800,400,100,150);
-    gs.x = pt.y = 0;
-    this.s.addChild(gs);
-
-    var as = new createjs.Shape();
-    as.graphics.beginFill("#000000").rect(975,400,100,150);
-    as.x = pt.y = 0;
-    this.s.addChild(as);
-
-    var keys = [ a,b,c,d,e,f,g,cs,ds,fs,gs];
-
-    this.s.update();
-
+    this.keyVal = new Array();
+    this.keyPressed = new Array();
+    for ( var i=0;i<12;i++){this.keyVal.push(0)};
+    this.beat = 0;
+    this.notes = 0;
+    var c = document.getElementById("pianoTeacher");
+    this.ctx = c.getContext("2d");
+    this.time = 0;
+    
   }
 
+    log( key, time ){
+        this.keyVal[key]=!this.keyVal[key];
+        this.time = time;
+        this.ctx.fillStyle = 'rgb(18, 60, 00)';
+        switch ( key ){
+            case 0:
+                this.ctx.fillRect(0,310,170,310);
+                this.ctx.fillStyle = 'black';
+                this.ctx.fillRect(115,310,110,210);
+                this.ctx.fillStyle = 'red';
+                break;
+            case 2:
+                this.ctx.fillRect(172,310,170,310);
+                this.ctx.fillStyle = 'black';
+                this.ctx.fillRect(115,310,110,210);
+                this.ctx.fillRect(285,310,120,210);
+                this.ctx.fillStyle = 'red';break;
+            case 4:
+                this.ctx.fillRect(343,310,170,310);
+                this.ctx.fillStyle = 'black';
+                this.ctx.fillRect(285,310,120,210);
+                this.ctx.fillStyle = 'red';break;
+            case 5:
+                this.ctx.fillRect(514,310,170,310);
+                this.ctx.fillStyle = 'black';
+                this.ctx.fillRect(630,310,120,210);
+                this.ctx.fillStyle = 'red';break;
+            case 7:
+                this.ctx.fillRect(685,310,170,310);
+                this.ctx.fillStyle = 'black';
+                this.ctx.fillRect(630,310,120,210);
+                this.ctx.fillRect(800,310,120,210);
+                this.ctx.fillStyle = 'red';break;
+            case 9:
+                this.ctx.fillRect(856,310,170,310);
+                this.ctx.fillStyle = 'black';
+                this.ctx.fillRect(970,310,120,210);
+                this.ctx.fillRect(800,310,120,210);
+                this.ctx.fillStyle = 'red';break;
+            case 11:
+                this.ctx.fillRect(1027,310,170,310);
+                this.ctx.fillStyle = 'black';
+                this.ctx.fillRect(970,310,120,210);
+                this.ctx.fillStyle = 'red';
+                break;
+            case 1:
+                this.ctx.fillStyle = 'black';
+                this.ctx.fillRect(116,310,120,210);break;
+            case 3:
+                this.ctx.fillStyle = 'black';
+                this.ctx.fillRect(287,310,120,210);break;
+            case 6:
+                this.ctx.fillStyle = 'black';
+                this.ctx.fillRect(630,310,120,210);break;
+            case 8:
+                this.ctx.fillStyle = 'black';
+                this.ctx.fillRect(800,310,120,210);break;
+            case 10:
+                this.ctx.fillStyle = 'black';
+                this.ctx.fillRect(970,310,120,210);break;
+        }
+        
+
+    }
+    updatePiano(key,time){
+        this.keyVal[key]=!this.keyVal[key];
+        this.time = time;
+        this.ctx.fillStyle = 'rgb(200,30,30)';
+        switch ( key ){
+            case 0:
+                this.ctx.fillRect(0,310,170,310);
+                this.ctx.fillStyle = 'black';
+                this.ctx.fillRect(115,310,110,210);
+                this.ctx.fillStyle = 'red';
+                break;
+            case 2:
+                this.ctx.fillRect(171,310,170,310);
+                this.ctx.fillStyle = 'black';
+                this.ctx.fillRect(115,310,110,210);
+                this.ctx.fillRect(285,310,120,210);
+                this.ctx.fillStyle = 'red';
+                break;
+            case 4:
+                this.ctx.fillRect(342,310,170,310);
+                this.ctx.fillStyle = 'black';
+                this.ctx.fillRect(285,310,120,210);
+                this.ctx.fillStyle = 'red';break;
+            case 5:
+                this.ctx.fillRect(513,310,170,310);
+                this.ctx.fillStyle = 'black';
+                this.ctx.fillRect(630,310,120,210);
+                this.ctx.fillStyle = 'red';break;
+            case 7:
+                this.ctx.fillRect(684,310,170,310);
+                this.ctx.fillStyle = 'black';
+                this.ctx.fillRect(630,310,120,210);
+                this.ctx.fillRect(800,310,120,210);
+                this.ctx.fillStyle = 'red';break;
+            case 9:
+                this.ctx.fillRect(855,310,170,310);
+                this.ctx.fillStyle = 'black';
+                this.ctx.fillRect(970,310,120,210);
+                this.ctx.fillRect(800,310,120,210);
+                this.ctx.fillStyle = 'red';break;
+            case 11:
+                this.ctx.fillRect(1026,310,170,310);
+                this.ctx.fillStyle = 'black';
+                this.ctx.fillRect(970,310,120,210);
+                this.ctx.fillStyle = 'red';break;
+            case 1:
+
+                this.ctx.fillRect(115,310,120,210);break;
+            case 3:
+                this.ctx.fillRect(285,310,120,210);break;
+            case 6:
+                this.ctx.fillRect(630,310,120,210);break;
+            case 8:
+                this.ctx.fillRect(800,310,120,210);break;
+            case 10:
+                this.ctx.fillRect(970,310,120,210);break;
+        }
+        
+    }
+    initPiano(){
+        this.ctx.fillStyle = 'rgb(0, 60, 180)';
+        this.ctx.fillRect(0, 0, 1410, 620);
+        this.ctx.fillStyle = 'black';
+        this.ctx.fillRect(1200,0,210,1410);
+        this.ctx.fillStyle = 'rgb(18, 60, 00)';
+        this.ctx.fillRect(0,310,171,310);
+        this.ctx.fillRect(171,310,171,310);
+        this.ctx.fillRect(342,310,171,310);
+        this.ctx.fillRect(513,310,171,310);
+        this.ctx.fillRect(684,310,171,310);
+        this.ctx.fillRect(855,310,171,310);
+        this.ctx.fillRect(1026,310,171,310);
+        this.ctx.fillStyle = 'rgb(81, 00, 30)';
+        this.ctx.strokeRect(0,310,171,310);
+        this.ctx.strokeRect(171,310,171,310);
+        this.ctx.strokeRect(342,310,171,310);
+        this.ctx.strokeRect(513,310,171,310);
+        this.ctx.strokeRect(684,310,171,310);
+        this.ctx.strokeRect(855,310,171,310);
+        this.ctx.strokeRect(1026,310,171,310);
+        this.ctx.fillStyle = 'black';
+        this.ctx.fillRect(115,310,110,210);
+        this.ctx.fillRect(285,310,110,210);
+        this.ctx.fillRect(630,310,110,210);
+        this.ctx.fillRect(800,310,110,210);
+        this.ctx.fillRect(970,310,110,210);
+    }
+    
 }
+
